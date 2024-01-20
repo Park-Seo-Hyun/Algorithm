f85363a6a69052b3de5ac75cd1781cb2f732033d
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-01-20 15:32:25 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int arr[30] = { 0 };
+    for (int i = 0; i < 28; i++) {
+        int num;
+        scanf("%d", &num);
+        arr[num - 1] = 1;
+    }
+    for (int j = 0; j < 30; j++) {
+        if (arr[j] == 0) {
+            printf("%d\n", j + 1);
+        }
+    }
+    return 0;
+}
