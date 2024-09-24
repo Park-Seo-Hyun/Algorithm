95596a4c5b12ab318136f881be8e2ea7a2e41100
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-09-24 20:26:00 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+
 int main(void) {
-    int num;
-    scanf("%d", &num);
-    for (int i = 0; i < num; i++) {
-        for (int j = 1; j < num - i; j++) {
-            printf(" ");
-        }
-        for (int k = 0; k < i + 1; k++) {
-            printf("*");
-        }
-        printf("\n");
-    }
-    return 0;
+
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		for (int j = i; j < n - 1; j++) {
+			printf(" ");
+		}
+		for (int k = 0; k < i + 1; k++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
