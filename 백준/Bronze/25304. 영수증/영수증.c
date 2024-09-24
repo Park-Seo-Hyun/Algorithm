bae14b5030c956bfcfdcfa33f87aea84d084c96f
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-09-24 19:41:36 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.c
+++ 백준/Bronze/25304. 영수증/영수증.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+
 int main(void) {
-    int total, amount, price, thing, hap = 0;
-    scanf("%d", &total);
-    scanf("%d", &amount);
-    for (int i = 0; i < amount; i++) {
-        scanf("%d %d", &price, &thing);
-        hap += price * thing;
-    }
-    if (total == hap) {
-        printf("Yes");
-    }
-    else {
-        printf("No");
-    }
+
+	int total, num, price, n, hap = 0;
+	scanf("%d", &total);
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
+		scanf("%d %d", &price, &n);
+		hap += price * n;
+	}
+	if (hap == total) printf("Yes");
+	else printf("No");
+
+	return 0;
 }
