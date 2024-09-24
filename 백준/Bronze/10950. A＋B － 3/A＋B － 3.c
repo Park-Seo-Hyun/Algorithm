071145c5edc128c89cb88144a34a920546e9a2e5
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-09-24 19:21:58 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-int main(void) {
-    int num;
-    int a, b;
-    scanf("%d", &num);
-    for (int i = 0; i < num; i++) {
-        scanf("%d %d", &a, &b);
-        printf("%d\n", a + b);
-    }
-    
-        
 
+int main(void) {
+
+	int num, A, B;
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
+		scanf("%d %d", &A, &B);
+		printf("%d\n", A + B);
+	}
+
+	return 0;
 }
