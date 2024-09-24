99d4ea52d737ea4f9ca3db6e6b2cf9c47db927b4
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-09-24 19:24:03 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.c
+++ 백준/Bronze/8393. 합/합.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 int main(void) {
-    int num, hap = 0;
-    scanf
-        ("%d", &num);
-    for (int i = 1; i < num + 1; i++) {
-        hap += i;
-    }
-    printf("%d", hap);
+
+	int num, hap = 0;
+	scanf("%d", &num);
+	for (int i = 1; i <= num; i++) {
+		hap += i;
+	}
+	printf("%d", hap);
+
+	return 0;
 }
