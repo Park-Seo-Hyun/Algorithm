8aa4029f8e762d2c73e8e3276adb5c4e54d2698b
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-09-24 20:05:25 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 int main(void) {
-    int num, a, b;
-    scanf("%d", &num);
-    for (int i = 1; i < num + 1; i++) {
-        scanf("%d %d", &a, &b);
-        printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
-    }
+
+	int num, a, b;
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d + %d = %d\n", i + 1, a, b, a + b);
+	}
+
+	return 0;
 }
