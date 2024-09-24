e2ea56ec9c6d516049da061906a4690641cc6b86
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-09-24 20:04:13 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
 int main(void) {
-    int num, a, b;
-    scanf("%d", &num);
-    for (int i = 1; i < num + 1; i++) {
-        scanf("%d %d", &a, &b);
-        printf("Case #%d: %d\n", i, a + b);
-    }
+
+	int num, a, b;
+	scanf("%d", &num);
+	for (int i = 0; i < num; i++) {
+		scanf("%d %d", &a, &b);
+		printf("Case #%d: %d\n", i + 1, a + b);
+	}
+
+	return 0;
 }
