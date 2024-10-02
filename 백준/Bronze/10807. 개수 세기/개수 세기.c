93a7d2998357ec5a186a996f9e0397c443243e28
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-03 06:50:18 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.c
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-    int n, num;
-    scanf("%d", &n);
-    int* arr = (int*)malloc(sizeof(int) * n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    int hap = 0;
-    scanf("%d", &num);
-    for (int i = 0; i < n; i++) {
-        if (num == arr[i]) {
-            hap++;
-        }
-    }
-    printf("%d", hap);
+	int num, size;
+	scanf("%d", &size);
+	int* arr = (int*)malloc(sizeof(int) * size);
+	for (int i = 0; i < size; i++) {
+		scanf("%d", &arr[i]);
+	}
+	int hap = 0;
+	scanf("%d", &num);
+	for (int i = 0; i < size; i++) {
+		if (num == arr[i]) {
+			hap++;
+		}
+	}
+	printf("%d", hap);
+	return 0;
 }
