9c2611711078f9ef6700607dbea04d57f00581cc
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-04 02:37:31 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.c
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(void) {
-    int n, m;
-    scanf("%d %d", &n, &m);
-    int* arr = (int*)malloc(sizeof(int)* n);
-    for (int i = 0; i < n; i++) {
-        arr[i] = i + 1;
-    }
-    for (int j = 0; j < m; j++) {
-        int I, J, temp;
-        scanf("%d %d", &I, &J);
-        temp = arr[I - 1];
-        arr[I - 1] = arr[J - 1];
-        arr[J - 1] = temp;
-    }
-    for (int k = 0; k < n; k++) {
-        printf("%d ", arr[k]);
-    }
-    
-    free(arr);
-    return 0;
+	int n, m;
+	scanf("%d %d", &n, &m);
+	int* arr = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++) {
+		arr[i] = i + 1;
+	}
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		int temp = arr[a - 1];
+		arr[a - 1] = arr[b - 1];
+		arr[b - 1] = temp;
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+	return 0;
 }
