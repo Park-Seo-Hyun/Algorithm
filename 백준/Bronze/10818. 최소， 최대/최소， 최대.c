d229d32027ee551e0d0f086846d260ac95904374
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-04 00:26:40 +0900

    [Bronze III] Title: 최소, 최대, Time: 160 ms, Memory: 5020 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.c
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
+
 int main(void) {
-    int n;
-    scanf("%d", &n);
-    int* arr = (int*)malloc(sizeof(int) * n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    int max = arr[0];
-    int min = arr[0];
-    for (int j = 0; j < n; j++) {
-        if (max < arr[j]) {
-            max = arr[j];
-        }
-        if (min > arr[j]) {
-            min = arr[j];
-        }
-    }
-    printf("%d ", min);
-    printf("%d", max);
-    
+	int n;
+	scanf("%d", &n);
+	int* arr = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	int max = arr[0], min = arr[0];
+	for (int i = 1; i < n; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+	}
+	printf("%d %d", min, max);
+	return 0;
 }
