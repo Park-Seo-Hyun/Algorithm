f6a11c59621d2134f5fba382c539401104a0a46b
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-03 22:42:20 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.c
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
+
 int main(void) {
-    int n, m, num;
-    scanf("%d %d", &n, &m);
-    int* arr = (int*)malloc(sizeof(int) * n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    for (int j = 0; j < n; j++) {
-        if (arr[j] < m) {
-            printf("%d ", arr[j]);
-        }
-    }
+	int n, x, num;
+	scanf("%d %d", &n, & x);
+	int* arr = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &arr[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		if (x > arr[i]) {
+			printf("%d ", arr[i]);
+		}
+	}
+	return 0;
 }
