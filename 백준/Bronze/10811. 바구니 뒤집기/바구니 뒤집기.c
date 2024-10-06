66d045c0259c3cd52801f68513b525e8b2b957b3
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-06 09:32:23 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.c
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(void) {
-    int n, m, I, J;
-    scanf("%d %d", &n, &m);
-    int* arr = (int*)malloc(sizeof(int) * n);
-    for (int p = 0; p < n; p++) {
-        arr[p] = p + 1;
-    }
-    for (int i = 0; i < m; i++) {
-        scanf("%d %d", &I, &J);
-        while (I < J) {
-            int temp = arr[I - 1];
-            arr[I - 1] = arr[J - 1];
-            arr[J - 1] = temp;
-            I++;
-            J--;
-        }
-    }
-    for (int k = 0; k < n; k++) {
-        printf("%d ", arr[k]);
-    }
-    free(arr);
-    return 0;
+	int n, m, I, J;
+	scanf("%d %d", &n, &m);
+	int* arr = (int*)malloc(sizeof(int) * n);
+	for (int i = 0; i < n; i++) {
+		arr[i] = i + 1;
+	}
+	while (m > 0) {
+		scanf("%d %d", &I, &J);
+		while (I < J) {
+			int temp = arr[I - 1];
+			arr[I - 1] = arr[J - 1];
+			arr[J - 1] = temp;
+			I++;
+			J--;
+		}
+		m--;
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
 }
