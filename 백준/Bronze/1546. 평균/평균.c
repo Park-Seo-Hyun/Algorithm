10425067b979309f9e31495cf638a59a517181a9
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-06 09:56:10 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.c
+++ 백준/Bronze/1546. 평균/평균.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(void) {
-    int n;
-    scanf("%d", &n);
-    int* arr = (int*)malloc(sizeof(int) * n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    int max = arr[0];
-    for (int j = 1; j < n; j++) {
-        if (max < arr[j]) {
-            max = arr[j];
-        }
-    }
-    double hap = 0;
-    for (int k = 0; k < n; k++) {
-        hap += (double)arr[k] / max * 100;
-    }
-    printf("%lf", hap / n);
-    free(arr);
-    return 0;
+	int num;
+	scanf("%d", &num);
+	int* arr = (int*)malloc(sizeof(int) * num);
+
+	for (int i = 0; i < num; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	int max = arr[0];
+	for(int i  = 0; i < num; i++) {
+		if (max < arr[i]) max = arr[i];
+	}
+
+	float total = 0;
+	for (int i = 0; i < num; i++) {
+		total += (float)arr[i] / max * 100;
+	}
+	printf("%f", total / num);
+
+
+
+	return 0;
 }
