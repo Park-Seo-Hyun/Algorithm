4a6edcbe19d1bff883f554007a317115ed5d9e46
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-10 02:02:07 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 1968 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.c
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -3,20 +3,18 @@
 
 int main(void) {
 	char arr[1000000];
-	int num = 0;
 	scanf("%[^\n]", arr);
-	int len = strlen(arr);
-	if (len == 1) {
-		if (arr[0] == ' ') {
-			printf("0\n");
+	int len = strlen(arr), num = 0;
+	if (arr[0] == ' ') {
+		if (len == 1) {
+			printf("%d", num);
 			return 0;
 		}
 	}
 	for (int i = 1; i < len - 1; i++) {
-		if (arr[i] == ' ') {
-			num++;
-		}
+		if (arr[i] == ' ') num++;
 	}
-	printf("%d\n", num + 1);
+	printf("%d", num + 1);
+
 	return 0;
 }
