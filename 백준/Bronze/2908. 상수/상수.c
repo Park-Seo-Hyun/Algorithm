83fd08fdc73447d9a033ac55be6f3c8069e28de2
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-11 02:32:44 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.c
+++ 백준/Bronze/2908. 상수/상수.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
+int change(char num[]);
+
 int main(void) {
 	char num1[4], num2[4];
 	scanf("%s %s", num1, num2);
-	int len1 = strlen(num1);
-	int len2 = strlen(num2);
-	for (int i = 0; i < len1 / 2; i++) {
-		char tmp = num1[i];
-		num1[i] = num1[len1 - 1 - i];
-		num1[len1 - 1 - i] = tmp;
-	}
-	int number1 = atoi(num1);
-	for (int j = 0; j < len2 / 2; j++) {
-		char tmp = num2[j];
-		num2[j] = num2[len2 - 1 - j];
-		num2[len2 - 1 - j] = tmp;
-	}
-	int number2 = atoi(num2);
-	if (number1 > number2) {
-		printf("%d", number1);
-	}
-	else {
-		printf("%d", number2);
-	}
+	int n1 = change(num1);
+	int n2 = change(num2);
+	if (n1 > n2) printf("%d", n1);
+	else printf("%d", n2);
+
+
 	return 0;
+}
+
+int change(char num[]) {
+	int len = strlen(num);
+	for (int i = 0; i < len / 2; i++) {
+		int temp = num[i];
+		num[i] = num[len - i - 1];
+		num[len - i - 1] = temp;
+	}
+
+	return atoi(num);
 }
