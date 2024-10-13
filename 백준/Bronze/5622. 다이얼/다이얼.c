1c1152e1e35df727f80bff7283aca9439daa6ea8
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-13 21:51:02 +0900

    [Bronze II] Title: 다이얼, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.c
+++ 백준/Bronze/5622. 다이얼/다이얼.c
@@ -2,35 +2,22 @@
 #include <string.h>
 
 int main(void) {
-	int sec = 0;
-	char alpha[16];
-	scanf("%s", alpha);
-	for (int i = 0; i < strlen(alpha); i++) {
-		if (alpha[i] >= 65 && alpha[i] <= 67) {
-			sec += 3;
-		}
-		else if (alpha[i] >= 68 && alpha[i] <= 70) {
-			sec += 4;
-		}
-		else if (alpha[i] >= 71 && alpha[i] <= 73) {
-			sec += 5;
-		}
-		else if (alpha[i] >= 74 && alpha[i] <= 76) {
-			sec += 6;
-		}
-		else if (alpha[i] >= 77 && alpha[i] <= 79) {
-			sec += 7;
-		}
-		else if (alpha[i] >= 80 && alpha[i] <= 83) {
-			sec += 8;
-		}
-		else if (alpha[i] >= 84 && alpha[i] <= 86) {
-			sec += 9;
-		}
-		else if (alpha[i] >= 87 && alpha[i] <= 90) {
-			sec += 10;
-		}
+	char arr[16];
+	scanf("%s", arr);
+	int time = 0;
+
+	for (int i = 0; i < strlen(arr); i++) {
+		char alpha = arr[i];
+		if (alpha >= 'A' && alpha <= 'C') time += 3;
+		else if (alpha >= 'D' && alpha <= 'F') time += 4;
+		else if (alpha >= 'G' && alpha <= 'I') time += 5;
+		else if (alpha >= 'J' && alpha <= 'L') time += 6;
+		else if (alpha >= 'M' && alpha <= 'O') time += 7;
+		else if (alpha >= 'P' && alpha <= 'S') time += 8;
+		else if (alpha >= 'T' && alpha <= 'V') time += 9;
+		else if (alpha >= 'W' && alpha <= 'Z') time += 10;
 	}
-	printf("%d", sec);
+	printf("%d", time);
+
 	return 0;
 }
