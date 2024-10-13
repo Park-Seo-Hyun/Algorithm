93a8a48bd393a666b33a54a7bae68a6a8851897f
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-14 03:22:26 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.c
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.c
@@ -2,17 +2,17 @@
 #include <string.h>
 
 int main(void) {
-	char* arr = (char*)malloc(sizeof(char) * 100);
-	int result = 1;
+	char arr[101];
 	scanf("%s", arr);
-	int len = strlen(arr) - 1;
-	for (int i = 0; i < (len + 1) / 2; i++) {
-		if (arr[i] != arr[len - i]) {
-			result = 0;
-			break;
+	int len = strlen(arr);
+	
+	for (int i = 0; i < len / 2; i++) {
+		if (arr[i] != arr[len - 1 - i]) {
+			printf("%d", 0);
+			return 0;
 		}
 	}
-	printf("%d", result);
-	free(arr);
+	printf("%d", 1);
+
 	return 0;
 }
