3292a00698914e285cb3234de770c5b6aa8cd1e6
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-10-18 11:34:34 +0900

    [Bronze III] Title: 배수와 약수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.py
@@ -0,0 +1,6 @@
+while(1):
+    x, y = map(int, input().split())
+    if(x == 0 and y == 0): exit()
+    if(y % x == 0): print("factor")
+    elif(x % y == 0): print("multiple")
+    else: print("neither")
