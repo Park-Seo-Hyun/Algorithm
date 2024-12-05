5ec3741a8fee94eb9fc50ea42c20a8b852404bd6
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-05 18:48:06 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,17 +1,15 @@
 n, m = map(int, input().split())
-arr = []
-
-for i in range(n):
-    arr.append(i + 1)
-
+arr = [0] * (n + 1)
+for i in range(n + 1):
+    arr[i] = i
 while(m > 0):
-    I, J = map(int, input().split())
-    while(I < J):
-        temp = arr[I - 1]
-        arr[I - 1] = arr[J- 1]
-        arr[J - 1] = temp
-        I += 1
-        J -= 1
-    m -=1
-for i in range(n):
+    i, j = map(int, input().split())
+    while(i < j):
+        tmp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = tmp
+        i += 1
+        j -= 1
+    m -= 1
+for i in range(1, n+1):
     print(arr[i], end=" ")
