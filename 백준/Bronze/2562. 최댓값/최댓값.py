17c6b9d2d8babc394a7acbbaffdfe2b03f5be022
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-05 18:00:32 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,14 +1,11 @@
 arr = []
-max = -1
-num = -1
-for i in range(0, 9):
-    value = int(input())
-    if (value < 1 or value > 99):
-        value = int(input())
-    arr.append(value)
-    if (max < value):
-        max = value
-        num = i + 1
-    
-print(max)
-print(num)
+for i in range(9):
+    num = int(input())
+    arr.append(num)
+max = arr[0]
+place = 1
+for i in range(1, 9):
+    if(arr[i] > max):
+        max = arr[i]
+        place = i + 1
+print('{}\n{}'.format(max, place))
