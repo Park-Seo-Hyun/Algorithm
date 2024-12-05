db62a1d9ca78fee5eec5edeaaec841fe642ae423
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-05 18:13:26 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.c
+++ 백준/Bronze/10810. 공 넣기/공 넣기.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-
+#include <stdlib.h>
 
 int main(void) {
 	int n, m;
 	scanf("%d %d", &n, &m);
+
 	int* arr = (int*)malloc(sizeof(int) * n);
-	memset(arr, 0, sizeof(int) * n);
+	for (int i = 0; i < n; i++) {
+		arr[i] = 0;
+	}
 	int a, b, c;
-	for (int i = 0; i < m; i++) {
+	while (m--) {
 		scanf("%d %d %d", &a, &b, &c);
-		for (int j = a - 1; j < b; j++) {
-			arr[j] = c;
+		for (int i = a - 1; i < b; i++) {
+			arr[i] = c;
 		}
 	}
 	for (int i = 0; i < n; i++) {
 		printf("%d ", arr[i]);
 	}
-	return 0;
+	return 0; 
 }
