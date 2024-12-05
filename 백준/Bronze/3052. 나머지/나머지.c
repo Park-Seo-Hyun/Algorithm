3115065574bda62378aca1fd537e7f62f2870ca8
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-05 18:36:31 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.c
+++ 백준/Bronze/3052. 나머지/나머지.c
@@ -1,23 +1,19 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 
 int main(void) {
 	int arr[10];
+	int total = 0, num;
 	for (int i = 0; i < 10; i++) {
-		scanf("%d", &arr[i]);
-		arr[i] %= 42;
+		scanf("%d", &num);
+		arr[i] = num % 42;
 	}
-	int result = 0;
 	for (int i = 0; i < 10; i++) {
 		int hap = 0;
-		for (int j = 0; j < i; j++) {
+		for (int j = i + 1; j < 10; j++) {
 			if (arr[i] == arr[j]) hap++;
 		}
-		if (hap == 0) result++;
+		if (hap == 0) total++;
 	}
-	printf("%d", result);
-	return 0;
+	printf("%d", total);
+	return 0; 
 }
