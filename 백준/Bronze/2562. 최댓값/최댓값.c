0e656fe789bcd694a8acf9aef6ee971c52cda911
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-05 17:58:37 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.c
+++ 백준/Bronze/2562. 최댓값/최댓값.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
-
 int main(void) {
-	int arr[9];
-	int max = arr[0];
-	int num = 0;
+	int arr[9] = { 0, };
 	for (int i = 0; i < 9; i++) {
 		scanf("%d", &arr[i]);
-		if (arr[i] < 1 || arr[i] > 99) {
-			scanf("%d", &arr[i]);
-		}
-		if (max < arr[i]) {
+	}
+	int max = arr[0], place = 1;
+	for (int i = 1; i < 9; i++) {
+		if (arr[i] > max) {
 			max = arr[i];
-			num = i + 1;
+			place = i + 1;
 		}
 	}
-	printf("%d\n", max);
-	printf("%d", num);
-	return 0;
+	printf("%d\n%d", max, place);
+	return 0; 
 }
