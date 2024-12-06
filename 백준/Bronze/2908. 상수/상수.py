99030a19e1bb0d6cc8ef04349789af18c770187c
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-06 15:01:18 +0900

    [Bronze II] Title: 상수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,16 +1,4 @@
-def change(num):
-    length = len(num)
-    for i in range(0, length // 2):
-        temp = num[i]
-        num[i] = num[length - i - 1]
-        num[length - i - 1] = temp
-
-    return int(''.join(num))
-
-num1, num2 = map(list, input().split())
-n1 = change(num1)
-n2 = change(num2)
-if (n1 > n2):
-    print(n1)
-else:
-    print(n2)
+n, m = input().split()
+n1 = 100 * int(n[2]) + 10 * int(n[1]) + int(n[0])
+m1 = 100 * int(m[2]) + 10 * int(m[1]) + int(m[0])
+print(max(n1, m1))
