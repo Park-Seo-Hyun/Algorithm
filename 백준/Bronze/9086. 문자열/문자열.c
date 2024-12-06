585c51882bea68d947ca0bfd066548fd59a591b3
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-06 12:49:25 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.c
+++ 백준/Bronze/9086. 문자열/문자열.c
@@ -4,12 +4,11 @@
 
 int main(void) {
 	int n;
-	char arr[1000];
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
+	char arr[1000];
+	while (n--) {
 		scanf("%s", arr);
 		printf("%c%c\n", arr[0], arr[strlen(arr) - 1]);
 	}
-
-	return 0;
+	return 0; 
 }
