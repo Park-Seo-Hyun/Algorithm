a130af3015af45200179bdcde158448f8c5c2dba
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-10 01:44:17 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,17 +1,16 @@
 arr = [[0] * 100 for i in range(100)]
 
-n =  int(input())
+n = int(input())
 while(n > 0):
-    x, y = map(int, input().split())
-    for i in range(x-1, x + 9):
-        for j in range(y - 1, y + 9):
+    a, b = map(int, input().split())
+    for i in range(a - 1, a + 9):
+        for j in range(b - 1, b + 9):
             arr[i][j] = 1
     n -= 1
-
+    
 cnt = 0
 for i in range(100):
     for j in range(100):
         if(arr[i][j] == 1):
-            cnt +=1
-
+            cnt += 1
 print(cnt)
