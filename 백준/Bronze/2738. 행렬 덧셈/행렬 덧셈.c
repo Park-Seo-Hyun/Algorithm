f30f76c9a70587cfc992a807de709c2b3fa92bf6
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-09 23:15:33 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.c
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main(void) {
 	int n, m;
 	scanf("%d %d", &n, &m);
-
-	int** arr1 = (int**)malloc(sizeof(int*) * n);
-	int** arr2 = (int**)malloc(sizeof(int*) * n);
-	for (int i = 0; i < n; i++) {
-		arr1[i] = (int*)malloc(sizeof(int) * m);
-		arr2[i] = (int*)malloc(sizeof(int) * m);
-	}
+	int arr1[100][100];
+	int arr2[100][100];
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			scanf("%d", &arr1[i][j]);
 		}
 	}
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			scanf("%d", &arr2[i][j]);
 		}
 	}
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			printf("%d ", arr1[i][j] + arr2[i][j]);
 		}
 		printf("\n");
 	}
-							 
-	return 0;				 
+	
+
+	return 0; 
 }
