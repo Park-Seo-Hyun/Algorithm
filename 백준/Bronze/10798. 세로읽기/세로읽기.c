c9819fe5e7046c8f80e93b79304e55e3391bf0cc
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-10 01:26:11 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.c
+++ 백준/Bronze/10798. 세로읽기/세로읽기.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+
 int main(void) {
 	char arr[5][16];
 	for (int i = 0; i < 5; i++) {
 		scanf("%15s", arr[i]);
 	}
-	for (int j = 0; j < 16; j++) {
-		for (int i = 0; i < 5; i++) {
-			if (j < strlen(arr[i])) {
-				printf("%c", arr[i][j]);
+
+	for (int i = 0; i < 16; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i < strlen(arr[j])) {
+				printf("%c", arr[j][i]);
 			}
 		}
 	}
-							 
-	return 0;				 
-}	
+	return 0; 
+}
