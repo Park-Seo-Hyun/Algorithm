1cb43fd6004c0c3991f2c8af87924ef315bcacef
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-09 23:54:15 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.c
+++ 백준/Bronze/2566. 최댓값/최댓값.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(void) {
 	int arr[9][9];
-	int row = 1, colum = 1, max = 0;
+	int max = 0, x = 1, y = 1;
 	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++) {
 			scanf("%d", &arr[i][j]);
 			if (max < arr[i][j]) {
 				max = arr[i][j];
-				row = i + 1;
-				colum = j + 1;
+				x = i + 1;
+				y = j + 1;
 			}
 		}
 	}
-	printf("%d\n%d %d", max, row, colum);
-							 
-	return 0;				 
-}		
+	printf("%d\n%d %d", max, x, y);
+	
+	return 0; 
+}
