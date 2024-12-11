9922d23c18ae5c4370645923afe1938e8ae9472f
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-11 19:23:09 +0900

    [Bronze I] Title: 약수들의 합, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.c
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.c
@@ -2,30 +2,29 @@
 
 int main(void) {
 	int n;
-
+	
 	while (1) {
-		int arr[10000] = { 0, };
-		int hap = 0, cnt = 0;
 		scanf("%d", &n);
-		if (n == -1) return 0;
+
+		if (n == -1) break;
+		int arr[10000];
+		int sum = 0, index = 0;
 		for (int i = 1; i < n; i++) {
 			if (n % i == 0) {
-				arr[cnt] = i;
-				hap += i;
-				cnt += 1;
+				sum += i;
+				arr[index++] = i;
 			}
 		}
-		if (hap == n) {
-			printf("%d = ", n);
-			for (int i = 0; i < cnt - 1; i++) {
-				printf("%d + ", arr[i]);
+		if (sum == n) {
+			printf("%d = %d", n, arr[0]);
+			for (int i = 1; i < index; i++) {
+				printf(" + %d", arr[i]);
 			}
-			printf("%d\n", arr[cnt - 1]);
+			printf("\n");
 		}
 		else {
 			printf("%d is NOT perfect.\n", n);
 		}
 	}
-							 
-	return 0;				 
-}	
+	return 0; 
+}
