aebb17ce1c97f8607e9667056190950fad689daf
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-12 19:41:04 +0900

    [Bronze III] Title: 대지, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.c
+++ 백준/Bronze/9063. 대지/대지.c
@@ -2,20 +2,20 @@
 #include <limits.h>
 
 int main(void) {
-	int n, x, y;
-	int max_x = INT_MIN, min_x = INT_MAX;
-	int max_y = INT_MIN, min_y = INT_MAX;
+	int x, y;
+	int min_x = INT_MAX, min_y = INT_MAX;
+	int max_x = INT_MIN, max_y = INT_MIN;
+	int n;
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
+	while (n--) {
 		scanf("%d %d", &x, &y);
-		if (x > max_x) max_x = x;
 		if (x < min_x) min_x = x;
-		if (y > max_y) max_y = y;
+		if (x > max_x) max_x = x;
 		if (y < min_y) min_y = y;
-
+		if (y > max_y) max_y = y;
 	}
 	printf("%d", (max_x - min_x) * (max_y - min_y));
 
-
-	return 0;
+	
+	return 0; 
 }
