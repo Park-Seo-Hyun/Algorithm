51ad072b8ae09a3c84dfbd87bda34c7ba86de5db
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-13 20:47:20 +0900

    [Silver V] Title: 최소공배수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.py
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -1,7 +1,8 @@
-import math
+def gcd(a, b):
+    if (b == 0):
+        return a
+    else:
+        return gcd(b, a % b)
 
-def LCM(a, b):
-    return a * b / math.gcd(a, b)
-    
 a, b = map(int, input().split())
-print('{}'.format(int(LCM(a, b))))
+print(a * b // gcd(a, b))
