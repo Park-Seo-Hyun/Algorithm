4df9a6a80a52188e357e2cefb67db8568a176f65
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-15 16:21:13 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 64 ms, Memory: 5020 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.c
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #define LIMIT (1000000)
 
-int goldPart(int n, int* arr);
-
 int main(void) {
-	int* arr = (int*)malloc(sizeof(int) * LIMIT + 1);
-	memset(arr, 1, sizeof(int) * LIMIT + 1);
-	arr[0] = arr[1] = 0;
-	for (int i = 2; i < LIMIT + 1; i++) {
+    int* arr = (int*)malloc(sizeof(int) * (LIMIT + 1));
+    memset(arr, 1, sizeof(int) * (LIMIT + 1));
+    arr[0] = 0, arr[1] = 0;
+	for (int i = 2; i < (int)sqrt(LIMIT) + 1; i++) {
 		int j = 2;
-		while (i * j <= LIMIT) {
+		while (i * j < LIMIT + 1) {
 			arr[i * j] = 0;
 			j++;
 		}
 	}
-	int t, n;
-	scanf("%d", &t);
-	while (t > 0) {
-		scanf("%d", &n);
-		printf("%d\n", goldPart(n, arr));
-		t--;
-	}
-	
-	return 0;
-}
 
-int goldPart(int n, int* arr) {
-	int hap = 0;
-	for (int i = 2; i <= n / 2; i++) {
-		int j = n - i;
-		if (arr[i] && arr[j]) hap++;
+	int n, num;
+	scanf("%d", &n);
+	while (n--) {
+		scanf("%d", &num);
+		int cnt = 0;
+		for (int i = 2; i < num / 2 + 1; i++) {
+			int j = num - i;
+			if (arr[i] && arr[j]) cnt++;
+		}
+		printf("%d\n", cnt);
 	}
-	return hap;
+    free(arr);
+    return 0;
 }
