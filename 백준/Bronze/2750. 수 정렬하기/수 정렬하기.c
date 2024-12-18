3a8778c7f2a083c21d1a61d2dc01dab27e58bae0
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-18 17:13:12 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.c
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define SWAP(x,y,z) ((z) = (x), (x) = (y), (y) = (z))
-
-void bubble_sort(int arr[], int n);
-
-int main(void) {
-	int n;
-	scanf("%d", &n);
-	int* arr = (int*)malloc(sizeof(int) * n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
-	}
-	bubble_sort(arr, n);
-	for (int i = 0; i < n; i++) {
-		printf("%d\n", arr[i]);
-	}
-	
-	
-	return 0;
+int compare(const void* a, const void* b) {
+    return *(int*)a - *(int*)b;
 }
 
-void bubble_sort(int arr[], int n) {
-	int temp;
-	for (int i = n - 1; i > 0; i--) {
-		for (int j = 0; j < i; j++) {
-			if (arr[j] > arr[j + 1]) {
-				SWAP(arr[j], arr[j + 1], temp);
-			}
-		}
-	}
+int main(void) {
+    int n, num;
+    scanf("%d", &n);
+    int* arr = (int*)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    qsort(arr, n, sizeof(int), compare);
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr[i]);
+    }
+
+
+    return 0;
 }
