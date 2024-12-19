31dc6874e448dd732781120661bf850254af7963
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-19 20:42:55 +0900

    [Silver II] Title: 좌표 압축, Time: 492 ms, Memory: 16748 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.c
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.c
@@ -2,42 +2,42 @@
 #include <stdlib.h>
 #include <limits.h>
 
-typedef struct {
-	int x;
-	int index;
-} Place;
+typedef struct Position {
+    int x;
+    int index;
+}Position;
 
 int compare(const void* a, const void* b) {
-	Place A = *(Place*)a;
-	Place B = *(Place*)b;
-	if (A.x > B.x) return 1;
-	else if (A.x < B.x) return -1;
-	else return 0;
+    Position X = *(Position*)a;
+    Position Y = *(Position*)b;
+    return X.x - Y.x;
 }
 
-
 int main(void) {
-	int n;
-	scanf("%d", &n);
-	Place* arr = (Place*)malloc(sizeof(Place) * n);
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i].x);
-		arr[i].index = i;
-	}
-	qsort(arr, n, sizeof(Place), compare);
-	int* result = (int*)malloc(sizeof(int) * n);
-	int cnt = -1, min = INT_MIN;
-	for (int i = 0; i < n; i++) {
-		if (arr[i].x != min) {
-			min = arr[i].x;
-			cnt++;
-		}
-		result[arr[i].index] = cnt;
-	}
-	for (int i = 0; i < n; i++) {
-		printf("%d ", result[i]);
-	}
-	
+    int n;
+    scanf("%d", &n);
+    Position* position;
+    position = (Position*)malloc(sizeof(Position) * n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &position[i].x);
+        position[i].index =i;
+    }
+    qsort(position, n, sizeof(Position), compare);
 
-	return 0;
+    int* arr = (int*)malloc(sizeof(int) * n);
+    int cnt = -1, min = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (position[i].x != min) {
+            min = position[i].x;
+            cnt++;
+        }
+        arr[position[i].index] = cnt;
+    }
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    free(position);
+    free(arr);
+    
+    return 0;
 }
