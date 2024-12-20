fe9c4274d41ca9ee545089780bb054019a7ef4f1
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-20 21:45:26 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 8 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.c
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.c
@@ -2,32 +2,28 @@
 #include <string.h>
 
 int cnt = 0;
-int recursion(const char* s, int l, int r);
-int isPalindrome(const char* s);
 
-int main(void) {
-	int n;
-	char arr[1001];
-
-	scanf("%d", &n);
-
-	while (n--) {
-		scanf("%s", arr);
-		printf("%d ", isPalindrome(arr));
-		printf("%d\n", cnt);
-	}
-
-	return 0;
-}
-
-int recursion(const char* s, int l, int r) {
+int recursion(char* s, int l, int r) {
 	cnt++;
 	if (l >= r) return 1;
 	else if (s[l] != s[r]) return 0;
 	else return recursion(s, l + 1, r - 1);
 }
 
-int isPalindrome(const char* s){
+int isPalindrome(char* s) {
 	cnt = 0;
 	return recursion(s, 0, strlen(s) - 1);
+}
+
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+
+	char arr[1001];
+	while (n--) {
+		scanf("%s", arr);
+		printf("%d ", isPalindrome(arr));
+		printf("%d\n", cnt);
+	}
 }
