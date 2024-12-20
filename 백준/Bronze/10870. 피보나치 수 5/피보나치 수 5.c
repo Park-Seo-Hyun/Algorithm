46a9d5e2aa55357c11dbd9011231d62a8b019573
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2024-12-20 21:17:22 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.c
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
-int fibo(int n);
+int fibo(int n) {
+	if (n == 0 || n == 1) return n;
+	else {
+		int tmp, last = 0, current = 1;
+		for (int i = 2; i < n + 1; i++) {
+			tmp = last + current;
+			last = current;
+			current = tmp;
+		}
+		return current;
+	}
+}
+
 
 int main(void) {
 	int n;
 	scanf("%d", &n);
 	printf("%d", fibo(n));
 
-
-	return 0;
-}
-
-int fibo(int n) {
-	if (n == 0) return 0;
-	else if (n == 1) return 1;
-	else return fibo(n - 2) + fibo(n - 1);
 }
