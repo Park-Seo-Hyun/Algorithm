ef4543b396e0bd5faf0043ee396d86a19dd16c5d
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2025-01-08 04:11:47 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.c
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 
-void change(int n, int m) {
+void change(int n, int b) {
 	if (n > 0) {
-		change(n / m, m);
-		int remain = n % m;
-		if (remain >= 10 && remain <= 35) {
-			printf("%c", remain + 55);
+		change(n / b, b);
+		int num = n % b;
+		if (num >= 10 && num <= 35) {
+			num += 55;
+			printf("%c", num);
 		}
-		else {
-			printf("%d", remain);
-		}
+		else printf("%d", num);
 	}
 }
 
 int main(void) {
-	int n, m;
-	scanf("%d %d", &n, &m);
-	change(n, m);
-	
-	return 0; 
+	int n, b;
+	scanf("%d %d", &n, &b);
+	change(n, b);
+
+	return 0;
 }
