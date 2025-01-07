2922fc102407a817d2a1aec53e7de087015d154a
Park-Seo-Hyun <121882902+Park-Seo-Hyun@users.noreply.github.com>
2025-01-08 03:57:22 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.c
+++ 백준/Bronze/1934. 최소공배수/최소공배수.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 int gcd(int a, int b) {
-    if (b == 0) return a;
-    else return gcd(b, a % b);
+	if (b == 0) return a;
+	else return gcd(b, a % b);
 }
 
-
 int main(void) {
-    int n, a, b;
-    scanf("%d", &n);
-    while (n--) {
-        scanf("%d %d", &a, &b);
-        printf("%d\n", a * b / gcd(a, b));
-    }
-
-    return 0;
+	int n, x, y;
+	scanf("%d", &n);
+	while (n--) {
+		scanf("%d %d", &x, &y);
+		printf("%d\n", x * y / gcd(x, y));
+	}
+	return 0;
 }
